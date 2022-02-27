aec683e9b894f771bf7063b94ccac547b482342b
Yiming Zuo <yz7608@node101.ionic.cs.princeton.edu>
2022-02-26 20:47:16 -0500

    noise exp

--- batch_train.sh
+++ batch_train.sh
@@ -8,15 +8,17 @@ shape=$2 # command line input
 if [ "$dataset" == 'LLFF' ]
 then
     datadir='/n/fs/pvl-viewsyn/llff/colmap_dense'
-    mod='baseline_lowres_batching'
+    # mod='baseline_lowres_batching'
+    mod='baseline_highres_nobatch'
+    #mod='baseline_4096_highres_batching'
 
     python run_nerf.py --expname "LLFF_${mod}_${shape}" --basedir /n/fs/pvl-viewsyn/nerf/logs \
     --datadir "${datadir}/${shape}" --dataset_type llff \
-    --use_viewdirs --N_samples 64 --N_importance 64 --N_rand 1024 \
-    --llffhold 5 --height 300 --raw_noise_std 1e0 \
+    --use_viewdirs --N_samples 64 --N_importance 128 --N_rand 1024 \
+    --llffhold 5 --height 600 --raw_noise_std 1e0 \
     --N_iters 200000 \
-    --i_video 1000000 --i_weights 50000 --i_testset 50000 \
-    # --no_batching
+    --i_video 1000000 --i_weights 20000 --i_testset 20000 \
+    --no_batching
 
 elif [ "$dataset" == 'DTUHR' ]
 then
@@ -35,3 +37,13 @@ else
    echo "unsupported dataset"
    exit 1
 fi
+
+
+# highres-no batch
+#python run_nerf.py --expname "LLFF_${mod}_${shape}" --basedir /n/fs/pvl-viewsyn/nerf/logs \
+#    --datadir "${datadir}/${shape}" --dataset_type llff \
+#    --use_viewdirs --N_samples 64 --N_importance 128 --N_rand 1024 \
+#    --llffhold 5 --height 600 --raw_noise_std 1e0 \
+#    --N_iters 200000 \
+#    --i_video 1000000 --i_weights 20000 --i_testset 20000 \
+#    --no_batching
--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -313,3 +313,86 @@ def write_pfm(file: str, image, scale=1):
 
         image.tofile(f)
 
+# adapted from barf:
+# https://github.com/chenhsuanlin/bundle-adjusting-NeRF/blob/f04e37cc3417bab25d71ccd9146bf696534ff3b1/camera.py#L83
+class Lie():
+    """
+    Lie algebra for SO(3) and SE(3) operations in PyTorch
+    """
+
+    def so3_to_SO3(self,w): # [...,3]
+        wx = self.skew_symmetric(w)
+        theta = w.norm(dim=-1)[...,None,None]
+        I = torch.eye(3,device=w.device,dtype=torch.float32)
+        A = self.taylor_A(theta)
+        B = self.taylor_B(theta)
+        R = I+A*wx+B*wx@wx
+        return R
+
+    def SO3_to_so3(self,R,eps=1e-7): # [...,3,3]
+        trace = R[...,0,0]+R[...,1,1]+R[...,2,2]
+        theta = ((trace-1)/2).clamp(-1+eps,1-eps).acos_()[...,None,None]%np.pi # ln(R) will explode if theta==pi
+        lnR = 1/(2*self.taylor_A(theta)+1e-8)*(R-R.transpose(-2,-1)) # FIXME: wei-chiu finds it weird
+        w0,w1,w2 = lnR[...,2,1],lnR[...,0,2],lnR[...,1,0]
+        w = torch.stack([w0,w1,w2],dim=-1)
+        return w
+
+    def se3_to_SE3(self,wu): # [...,6]
+        w,u = wu.split([3,3],dim=-1)
+        wx = self.skew_symmetric(w)
+        theta = w.norm(dim=-1)[...,None,None]
+        I = torch.eye(3,device=w.device,dtype=torch.float32)
+        A = self.taylor_A(theta)
+        B = self.taylor_B(theta)
+        C = self.taylor_C(theta)
+        R = I+A*wx+B*wx@wx
+        V = I+B*wx+C*wx@wx
+        Rt = torch.cat([R,(V@u[...,None])],dim=-1)
+        return Rt
+
+    def SE3_to_se3(self,Rt,eps=1e-8): # [...,3,4]
+        R,t = Rt.split([3,1],dim=-1)
+        w = self.SO3_to_so3(R)
+        wx = self.skew_symmetric(w)
+        theta = w.norm(dim=-1)[...,None,None]
+        I = torch.eye(3,device=w.device,dtype=torch.float32)
+        A = self.taylor_A(theta)
+        B = self.taylor_B(theta)
+        invV = I-0.5*wx+(1-A/(2*B))/(theta**2+eps)*wx@wx
+        u = (invV@t)[...,0]
+        wu = torch.cat([w,u],dim=-1)
+        return wu
+
+    def skew_symmetric(self,w):
+        w0,w1,w2 = w.unbind(dim=-1)
+        O = torch.zeros_like(w0)
+        wx = torch.stack([torch.stack([O,-w2,w1],dim=-1),
+                          torch.stack([w2,O,-w0],dim=-1),
+                          torch.stack([-w1,w0,O],dim=-1)],dim=-2)
+        return wx
+
+    def taylor_A(self,x,nth=10):
+        # Taylor expansion of sin(x)/x
+        ans = torch.zeros_like(x)
+        denom = 1.
+        for i in range(nth+1):
+            if i>0: denom *= (2*i)*(2*i+1)
+            ans = ans+(-1)**i*x**(2*i)/denom
+        return ans
+    def taylor_B(self,x,nth=10):
+        # Taylor expansion of (1-cos(x))/x**2
+        ans = torch.zeros_like(x)
+        denom = 1.
+        for i in range(nth+1):
+            denom *= (2*i+1)*(2*i+2)
+            ans = ans+(-1)**i*x**(2*i)/denom
+        return ans
+    def taylor_C(self,x,nth=10):
+        # Taylor expansion of (x-sin(x))/x**3
+        ans = torch.zeros_like(x)
+        denom = 1.
+        for i in range(nth+1):
+            denom *= (2*i+2)*(2*i+3)
+            ans = ans+(-1)**i*x**(2*i)/denom
+        return ans
+
